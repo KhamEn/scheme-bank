f7cbc8d380582d97006cc2dd64045432997989c3
Kham En <khamen28@gmail.com>
2022-11-12 05:32:01 -0500

    function: Implement the add palette function

---
+++ src/database/hooks/palettes-group/useAddPaletteMutation.js
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addDoc, collection } from "firebase/firestore";
+import db from "../../firestore-config";
+import { FIRESTORE_COLLECTION } from "../../Enums";
+import useGetCurrentSchemeIdQuery from "../useGetCurrentSchemeIdQuery";
+
+/*
+@param variablese.schemeId - the doc id of the scheme where the new palette will be added
+@param variables.paletteType - the doc id of the palette group where the new palette will be added
+@param variables.paletteName - the name of the new palette
+*/
+async function addPalette(variables) {
+  console.log(variables);
+  await addDoc(
+    collection(
+      db,
+      FIRESTORE_COLLECTION.schemes,
+      variables.schemeId,
+      variables.paletteType
+    ),
+    {
+      name: variables.paletteName,
+      colors: [],
+    }
+  );
+}
+
+function useAddPaletteMutation(paletteType) {
+  const queryClient = useQueryClient();
+  const { data } = useGetCurrentSchemeIdQuery();
+
+  return useMutation(addPalette, {
+    onSuccess: () => {
+      queryClient.invalidateQueries([data.id, paletteType]);
+    },
+  });
+}
+
+export default useAddPaletteMutation;
--- src/scheme-page/PaletteGroup.jsx
+++ src/scheme-page/PaletteGroup.jsx
@@ -1,8 +1,22 @@
+import useAddPaletteMutation from "../database/hooks/palettes-group/useAddPaletteMutation";
+import useGetCurrentSchemeIdQuery from "../database/hooks/useGetCurrentSchemeIdQuery";
 import useGetPalettesQuery from "../database/hooks/useGetPalettesQuery";
 import Palette from "./Palette";
 
 const PaletteGroup = ({ groupName }) => {
   const { data, isLoading } = useGetPalettesQuery(groupName);
+  const { data: currentSchemeId } = useGetCurrentSchemeIdQuery();
+  const { mutate } = useAddPaletteMutation(groupName);
+
+  function handleNewPaletteClick() {
+    const paletteName = "new paletteo";
+    const variables = {
+      schemeId: currentSchemeId.id,
+      paletteType: groupName,
+      paletteName: paletteName,
+    };
+    mutate(variables);
+  }
 
   function listPalettes() {
     const palettes = [];
@@ -26,6 +40,12 @@ const PaletteGroup = ({ groupName }) => {
   return (
     <div>
       <h1>{groupName}</h1>
+      <button
+        onClick={handleNewPaletteClick}
+        className=" border-2 border-green-500"
+      >
+        New Palette
+      </button>
       {!isLoading && listPalettes()}
     </div>
   );
