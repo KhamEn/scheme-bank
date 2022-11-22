dd3e58dc32f70d8fc733074238020aac96747d13
Kham En <khamen28@gmail.com>
2022-11-22 09:47:25 -0500

    Style rename palette component

--- src/components/ui/RenameModalDialog.jsx
+++ src/components/ui/RenameModalDialog.jsx
@@ -27,20 +27,36 @@ const RenameModalDialog = ({ isOpen, setIsOpen, originalName, rename }) => {
       className="relative z-50"
     >
       <Dialog.Backdrop
-        className={"fixed inset-0 bg-black/30"}
+        className={"fixed inset-0 bg-black/70"}
         aria-hidden="true"
       />
-      <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className=" bg-blue-500">
-          <Dialog.Title>Rename Palette</Dialog.Title>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <Dialog.Panel className="rounded-md border bg-gray-100 p-5">
+          <Dialog.Title className=" text-xl font-bold  text-blue-900 ">
+            Rename Palette
+          </Dialog.Title>
           <input
             autoFocus
             type="text"
             value={nameInput}
             onChange={(e) => setNameInput(e.target.value)}
+            maxLength="25"
+            className="mt-4 p-2"
           />
-          <button onClick={handleConfirmClick}>Confirm</button>
-          <button onClick={handleCancelClick}>Cancel</button>
+          <div className="mt-4 flex justify-evenly gap-2">
+            <button
+              onClick={handleConfirmClick}
+              className="btn btn-create w-full p-[2px]"
+            >
+              Confirm
+            </button>
+            <button
+              onClick={handleCancelClick}
+              className="btn btn-delete w-full p-[2px]"
+            >
+              Cancel
+            </button>
+          </div>
         </Dialog.Panel>
       </div>
     </Dialog>
