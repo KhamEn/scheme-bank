9133e84e2d1046a0afa36710f4b44b98a4913855
Kham En <khamen28@gmail.com>
2022-12-15 09:51:26 -0500

    FEATURE: Use a new starter scheme
    The new scheme is based on RefactorUI's example on building a color palette, and the colors are taken from TailwindCSS default color palettes.
    The new scheme is used to show what the app can do and to look more appealing to new users.

--- src/database/hooks/users/useCreateUserWithStarterSchemeMutation.js
+++ src/database/hooks/users/useCreateUserWithStarterSchemeMutation.js
@@ -27,7 +27,7 @@ async function createUserWithStarterScheme(variables) {
     firestoreCollection.SCHEMES
   );
   const newSchemeDocRef = await addDoc(schemesCollectionRef, {
-    name: "starter scheme",
+    name: "example",
   });
   await updateDoc(userDocRef, { currentSchemeId: newSchemeDocRef.id });
 
@@ -41,9 +41,19 @@ async function createUserWithStarterScheme(variables) {
     firestoreSubCollection.PRIMARY_PALETTES
   );
   await addDoc(primaryPalettesColRef, {
-    name: "prim",
-    colors: [],
-    colorNames: [],
+    name: "blue",
+    colors: [
+      "#dbeafe",
+      "#bfdbfe",
+      "#93c5fd",
+      "#60a5fa",
+      "#3b82f6",
+      "#2563eb",
+      "#1d4ed8",
+      "#1e40af",
+      "#1e3a8a",
+    ],
+    colorNames: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   });
   const neutralPalettesColRef = collection(
     db,
@@ -54,9 +64,19 @@ async function createUserWithStarterScheme(variables) {
     firestoreSubCollection.NEUTRAL_PALETTES
   );
   await addDoc(neutralPalettesColRef, {
-    name: "neut",
-    colors: [],
-    colorNames: [],
+    name: "gray",
+    colors: [
+      "#f3f4f6",
+      "#e5e7eb",
+      "#d1d5db",
+      "#9ca3af",
+      "#6b7280",
+      "#4b5563",
+      "#374151",
+      "#1f2937",
+      "#111827",
+    ],
+    colorNames: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   });
   const accentPalettesColRef = collection(
     db,
@@ -67,9 +87,34 @@ async function createUserWithStarterScheme(variables) {
     firestoreSubCollection.ACCENT_PALETTES
   );
   await addDoc(accentPalettesColRef, {
-    name: "acce",
-    colors: [],
-    colorNames: [],
+    name: "green",
+    colors: [
+      "#dcfce7",
+      "#bbf7d0",
+      "#86efac",
+      "#4ade80",
+      "#22c55e",
+      "#16a34a",
+      "#15803d",
+      "#166534",
+      "#14532d",
+    ],
+    colorNames: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  });
+  await addDoc(accentPalettesColRef, {
+    name: "red",
+    colors: [
+      "#fee2e2",
+      "#fecaca",
+      "#fca5a5",
+      "#f87171",
+      "#ef4444",
+      "#dc2626",
+      "#b91c1c",
+      "#991b1b",
+      "#7f1d1d",
+    ],
+    colorNames: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   });
 }
 
